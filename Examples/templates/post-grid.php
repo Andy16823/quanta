3d615aabee9fe28ba9de5330ba761aea7decba36
Andreas Wagner <wagnerandreas17@yahoo.de>
2025-01-06 22:36:22 +0100

    Remove old examples

--- Examples/templates/post-grid.php
+++
@@ -1,5 +0,0 @@
-<div class="grid">
-    <?php foreach($cards as $card): ?>
-        <?php $quanta->render_component("card", $card); ?>
-    <?php endforeach; ?>
-</div>
--- Examples/templates/user-entry.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <h3><?php echo $user_name; ?></h3>
-    <p><?php echo $user_id; ?> | <?php echo $phone; ?></p>
-</div>
